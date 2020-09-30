cb1482af0ad8e47b7a4fa2fe05b653dce5c93d47
Sasha Gavryshko <sasha.gavryshko@gmail.com>
2020-09-30 21:12:47 +0200

    spa empty components

---
+++ src/IManagerSPA/src/app/account/registration-form/registration-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrls: ['./registration-form.component.scss']
+})
+export class RegistrationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+    // https://github.com/mmacneil/AngularASPNETCoreAuthentication/tree/master/src/dotnetGigs
+}
